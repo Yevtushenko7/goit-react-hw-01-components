475d5b229f3f0310414387534e85881325bf176d
Yevhen Yevtushenko <korish140193@gmail.com>
2021-06-06 12:00:35 +0300

    .

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
 import TransactionHistory from './components/TransactionHistory/TransactionHistory';
-import user from './user.json';
-import statData from './statistical-data.json';
-import friends from './friends.json';
-import transactions from './transactions.json';
+import user from './data/user.json';
+import statData from './data/statistical-data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 
 const App = () => {
   return (
